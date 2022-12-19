c5a92c9e9138681facd80fce3de08060a6e8f067
fernanda freitas <idea.fernanda@gmail.com>
2022-12-19 15:49:52 +0000

    first commit

---
+++ src/components/Player/Index.jsx
@@ -0,0 +1,89 @@
+import './Player.scss'
+import {useEffect, useState} from 'react'
+import iconPlay from '../../images/icon-play.svg'
+import iconStop from '../../images/icon-stop.svg'
+
+const Player = () => {
+  const tracks = [
+    {
+      name: "Mekanın Sahibi",
+      artist: "Norm Ender",
+      cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/1.jpg",
+      source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/1.mp3",
+      url: "https://www.youtube.com/watch?v=z3wAjJXbYzA",
+      favorited: false
+    },
+    {
+      name: "Everybody Knows",
+      artist: "Leonard Cohen",
+      cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/2.jpg",
+      source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/2.mp3",
+      url: "https://www.youtube.com/watch?v=Lin-a2lTelg",
+      favorited: true
+    },
+    {
+      name: "Extreme Ways",
+      artist: "Moby",
+      cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/3.jpg",
+      source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/3.mp3",
+      url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
+      favorited: false
+    }
+  ]
+
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [audio, setAudio] = useState(null)
+  const [audioIndex, setAudioIndex] = useState(0)
+
+  useEffect(() => {
+    const audio = new Audio(tracks[audioIndex].source);
+    setAudio(audio)
+
+    return () => {
+      audio.pause()
+    }
+  }, [])
+
+  const play = () => {
+    setIsPlaying(true);
+    audio.play();
+  }
+
+  const pause = () => {
+    setIsPlaying(false);
+    audio.pause();
+  }
+
+  const playNext = () => {
+    const index = tracks.indexOf(audioIndex)
+    if (index !== tracks.length - 1) {
+      setAudioIndex(tracks[index + 1])
+    }  else {
+      setAudioIndex(tracks[0])
+    }
+  }
+
+  const playPrev = () => {
+    console.log('Play prev')
+  }
+
+  return (
+    <>
+      <button onClick={playPrev}>Prev</button>
+      {
+        !isPlaying? (
+          <button className='button-xl' onClick={play}>
+            <img src={iconPlay} alt="play music" />
+          </button>
+        ) : (
+          <button className='button-xl' onClick={pause}>
+            <img src={iconStop} alt="play music" />
+          </button>
+        )
+      }
+      <button onClick={playNext}>Next</button>
+    </>
+  )
+}
+
+export default Player;
