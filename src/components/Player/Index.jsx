fc1449d839f3000c525e5e219a37368e5c07005e
fernanda freitas <idea.fernanda@gmail.com>
2022-12-19 17:25:00 +0000

    next button logic implementation

--- src/components/Player/Index.jsx
+++ src/components/Player/Index.jsx
@@ -28,6 +28,54 @@ const Player = () => {
       source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/3.mp3",
       url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
       favorited: false
+    },
+    {
+      name: "Butterflies",
+      artist: "Sia",
+      cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/4.jpg",
+      source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/4.mp3",
+      url: "https://www.youtube.com/watch?v=kYgGwWYOd9Y",
+      favorited: false
+    },
+    {
+      name: "The Final Victory",
+      artist: "Haggard",
+      cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/5.jpg",
+      source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/5.mp3",
+      url: "https://www.youtube.com/watch?v=0WlpALnQdN8",
+      favorited: true
+    },
+    {
+      name: "Genius ft. Sia, Diplo, Labrinth",
+      artist: "LSD",
+      cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/6.jpg",
+      source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/6.mp3",
+      url: "https://www.youtube.com/watch?v=HhoATZ1Imtw",
+      favorited: false
+    },
+    {
+      name: "The Comeback Kid",
+      artist: "Lindi Ortega",
+      cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/7.jpg",
+      source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/7.mp3",
+      url: "https://www.youtube.com/watch?v=me6aoX0wCV8",
+      favorited: true
+    },
+    {
+      name: "Overdose",
+      artist: "Grandson",
+      cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/8.jpg",
+      source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/8.mp3",
+      url: "https://www.youtube.com/watch?v=00-Rl3Jlx-o",
+      favorited: false
+    },
+    {
+      name: "Rag'n'Bone Man",
+      artist: "Human",
+      cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/9.jpg",
+      source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/9.mp3",
+      url: "https://www.youtube.com/watch?v=L3wKzyIN1yk",
+      favorited: false
     }
   ]
 
@@ -54,8 +102,10 @@ const Player = () => {
     audio.pause();
   }
 
+
   const playNext = () => {
     audio.pause()
+    setIsPlaying(true)
 
     if (currentSongIndex < tracks.length - 1) {
       setcurrentSongIndex(currentSongIndex + 1)
@@ -71,11 +121,25 @@ const Player = () => {
   }
 
   const playPrev = () => {
-    console.log('Play prev')
+    audio.pause()
+    setIsPlaying(true)
+
+    if (currentSongIndex >= 0) {
+      setcurrentSongIndex(currentSongIndex - 1)
+      const prevAudio = new Audio(tracks[currentSongIndex].source);
+      setAudio(prevAudio)
+      prevAudio.play()
+    } else {
+      setcurrentSongIndex(0)
+      const prevAudio = Audio(tracks[currentSongIndex].source)
+      setAudio(prevAudio)
+      prevAudio.play()
+    }
   }
 
   return (
     <>
+      <img src={tracks[currentSongIndex].cover} alt="cover" />
       <button onClick={playPrev}>Prev</button>
       {
         !isPlaying? (
